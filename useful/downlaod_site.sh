49689ac87d12d0c1bed0a64e3da33ba060ce6cb3
Maxim Nikitenko <iam@sets88.com>
2022-11-04 16:47:29 +0200

    update site downloader script

--- useful/downlaod_site.sh
+++ useful/downlaod_site.sh
@@ -1,7 +1,18 @@
-if [ -z $1 ]; then
+usage() {
     echo "Site downloader\nUsage $0 <URL>"
     exit 1
+}
+
+if [ -z "$1" ]; then
+    usage
 fi
+
+domain=`echo $1 awk -F/ '{print $3}'`
+
+if [ -z "$domain" ]; then
+    usage
+fi
+
 wget \
      --recursive \
      --no-clobber \
@@ -9,6 +20,6 @@ wget \
      --html-extension \
      --convert-links \
      --restrict-file-names=windows \
-     --domains $1 \
+     --domains $domain \
      --no-parent \
          $1
