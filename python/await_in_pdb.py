c6b5880cef557557f2c2eceeaa9b24af7cc9b0b5
Maxim Nikitenko <iam@sets88.com>
2024-11-15 12:51:37 +0200

    add timeout

--- python/await_in_pdb.py
+++ python/await_in_pdb.py
@@ -6,22 +6,32 @@ async def fn():
     return 'test'
 
 
-def awaitc(coro):
+def awaitc(coro, timeout=None):
     import threading
 
-    result = {"result": None}
+    result = {"result": None, "exception": None}
 
-    def run_coro(result):
+    def run_coro(result, timeout):
         loop = asyncio.new_event_loop()
-        result['result'] = loop.run_until_complete(coro)
+        try:
+            if timeout:
+                result['result'] = loop.run_until_complete(asyncio.wait_for(coro, timeout))
+            else:
+                result['result'] = loop.run_until_complete(coro)
+        except Exception as exc:
+            result['exception'] = exc
 
-    t = threading.Thread(target=run_coro, args=(result,))
+    t = threading.Thread(target=run_coro, args=(result, timeout))
     t.start()
     t.join()
+
+    if result['exception']:
+        raise result['exception']
+
     return result['result']
 
 
-print(awaitc(fn()))
+print(awaitc(fn(), timeout=3))
 
 # (Pdb) print(await fn())
 # *** SyntaxError: 'await' outside function
