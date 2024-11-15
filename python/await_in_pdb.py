6e952a2a48d5a02725aa82e55195b4142a2793be
Maxim Nikitenko <iam@sets88.com>
2024-11-15 08:55:28 +0200

    add await_in_pdb.py python script

---
+++ python/await_in_pdb.py
@@ -0,0 +1,27 @@
+async def fn():
+    await asyncio.sleep(2)
+    return 'test'
+
+
+def awaitc(coro):
+    import asyncio
+    import threading
+
+    result = {"result": None}
+
+    def run_coro(result):
+        loop = asyncio.new_event_loop()
+        result['result'] = loop.run_until_complete(coro)
+
+    t = threading.Thread(target=run_coro, args=(result,))
+    t.start()
+    t.join()
+    return result['result']
+
+
+print(awaitc(fn()))
+
+# (Pdb) print(await fn())
+# *** SyntaxError: 'await' outside function
+# (Pdb++) print(awaitc(fn()))
+# test
